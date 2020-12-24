4f7ae842065372b013f935b5b7ac29fff9cddd40
Sofiane Bebert <bebertsofiane@gmail.com>
2020-12-24 11:25:38 +0000

    :construction: create vpc object

--- networking/__main__.py
+++ networking/__main__.py
@@ -1,52 +1,16 @@
-"""An AWS Python Pulumi program"""
+"""Manage the creation of VPC infrastructure and the peering relationships between them.
+
+
+"""
 
 import pulumi
 from pulumi_aws import ec2, get_availability_zones, get_ami, GetAmiFilterArgs
 
-# from vpc import Vpc
+from vpc import Vpc
 
-config = pulumi.Config()
-az_count = config.require_int("az_count")
+vpc = Vpc()
 
-tags = {"pulumi_managed": "true", "AutoOff": "True"}
-
-zones = get_availability_zones().names[:az_count]
-
-# TODO Create a VPC for the apps
-vpc = ec2.Vpc("educate-app-vpc", cidr_block="10.0.0.0/16")
-
-igw = ec2.InternetGateway("educate-app-igw", vpc_id=vpc.id)
-
-public_route_table = ec2.RouteTable(
-    "educate-app-rt",
-    routes=[ec2.RouteTableRouteArgs(cidr_block="0.0.0.0/0", gateway_id=igw.id)],
-    vpc_id=vpc.id,
-)
-
-public_subnet_ids = []
-
-for zone in zones:
-    # TODO Create 1 public and 1 private subnet
-    public_subnet = ec2.Subnet(
-        f"educate-public-subnet-{zone}",
-        assign_ipv6_address_on_creation=False,
-        vpc_id=vpc.id,
-        map_public_ip_on_launch=True,
-        cidr_block="10.0.1.0/24",
-        availability_zone=zone,
-        tags=tags,
-    )
-
-    ec2.RouteTableAssociation(
-        f"educate-app-public-rta-{zone}",
-        route_table_id=public_route_table.id,
-        subnet_id=public_subnet.id,
-    )
-
-    public_subnet_ids.append(public_subnet.id)
-
-
-pulumi.export("vpc_id", vpc.id)
+pulumi.export("vpc_id", vpc.get_id())
 
 size = "t2.micro"
 
@@ -58,7 +22,7 @@ ami = get_ami(
 
 group = ec2.SecurityGroup(
     "test-sg",
-    vpc_id=vpc.id,
+    vpc_id=vpc.get_id(),
     description="Enable HTTP access",
     ingress=[
         ec2.SecurityGroupIngressArgs(
@@ -79,14 +43,14 @@ nohup python -m SimpleHTTPServer 80 &
 server = ec2.Instance(
     "test-ec2-instance",
     instance_type=size,
-    subnet_id=public_subnet.id,
+    subnet_id=vpc.get_public_subnet_id(),
     vpc_security_group_ids=[group.id],
     user_data=user_data,
     ami=ami.id,
 )
 
-pulumi.export("vpc_id", vpc.id)
-pulumi.export("public_subnet_ids", public_subnet_ids)
+pulumi.export("vpc_id", vpc.get_id())
+pulumi.export("public_subnet_ids", vpc.get_public_subnet_id())
 pulumi.export("public_ip", server.public_ip)
 pulumi.export("public_dns", server.public_dns)
 
--- networking/vpc.py
+++ networking/vpc.py
@@ -1,27 +1,29 @@
 """This module defines a Pulumi component resource for encapsulating our best practices for building an AWS VPC.
 
 This includes:
-- Create the named VPC with appropriate tags
-- Create a minimum of 3 subnets across multiple availability zones
-- Create an internet gateway
-- Create an IPv6 egress gateway
-- Create a route table and associate the created subnets with it
-- Create a routing table to include the relevant peers and their networks
-- Create an RDS subnet group
+- TODO Create the named VPC with appropriate tags
+- TODO Create a minimum of 3 subnets across multiple availability zones
+- TODO Create an internet gateway
+- TODO Create an IPv6 egress gateway (Why??)
+- TODO  Create a route table and associate the created subnets with it
+- TODO Create a routing table to include the relevant peers and their networks
+- TODO Create an RDS subnet group (Why??)
 """
-from pulumi import ComponentResource
-from pulumi_aws import ec2
+from pulumi import ComponentResource, Config
+from pulumi_aws import ec2, get_availability_zones
 
 # TODO Vpc Component
-class Vpc(pulumi.ComponentResource):
+class Vpc(ComponentResource):
     """Pulumi component for building all of the necessary pieces of an AWS VPC.
 
     A component resource that encapsulates all of the standard practices of how the Dicey Tech
      Engineering team constructs and organizes VPC environments in AWS.
     """
 
-    def __init__():
-        """Build an AWS VPC with subnets, internet gateway, and routing table.
+    def __init__(self):
+        """
+        Build an AWS VPC with subnets, internet gateway, and routing table.
+
         :param vpc_config: Configuration object for customizing the created VPC and
             associated resources.
         :type vpc_config: OLVPCConfig
@@ -29,4 +31,51 @@ class Vpc(pulumi.ComponentResource):
             for handling things like AWS provider overrides.
         :type opts: Optional[ResourceOptions]
         """
-        super.__init__("dt:netowrking:VPC")
+        # super.__init__("dt:netowrking:VPC")
+
+        self.tags = {"pulumi_managed": "true", "AutoOff": "True"}
+
+        self.vpc = ec2.Vpc("educate-app-vpc", cidr_block="10.0.0.0/16")
+
+        self.igw = ec2.InternetGateway("educate-app-igw", vpc_id=self.vpc.id)
+
+        self.public_route_table = ec2.RouteTable(
+            "educate-app-rt",
+            routes=[
+                ec2.RouteTableRouteArgs(cidr_block="0.0.0.0/0", gateway_id=self.igw.id)
+            ],
+            vpc_id=self.vpc.id,
+        )
+
+        self.public_subnet_ids = []
+
+        config = Config("apps_vpc")
+        az_count = config.require_int("az_count")
+        zones = get_availability_zones().names[:az_count]
+
+        # TODO make subnet cird programmatically defined
+        for zone in zones:
+            # TODO Create 1 public and 1 private subnet
+            public_subnet = ec2.Subnet(
+                f"educate-public-subnet-{zone}",
+                assign_ipv6_address_on_creation=False,
+                vpc_id=self.vpc.id,
+                map_public_ip_on_launch=True,
+                cidr_block="10.0.1.0/24",
+                availability_zone=zone,
+                tags=self.tags,
+            )
+
+            ec2.RouteTableAssociation(
+                f"educate-app-public-rta-{zone}",
+                route_table_id=self.public_route_table.id,
+                subnet_id=public_subnet.id,
+            )
+
+            self.public_subnet_ids.append(public_subnet.id)
+
+    def get_id(self):
+        return self.vpc.id
+
+    def get_public_subnet_id(self):
+        return self.public_subnet_ids[0]
